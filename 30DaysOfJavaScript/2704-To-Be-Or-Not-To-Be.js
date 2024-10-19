55f667d937fa4a935e2c95ef930ea8d65f36fb4c
Andrew Ofuenweuche <chidi.andrew@gmail.com>
2024-10-19 13:53:35 +0100

    [2704] [Time Beats: 82.78%] [Memory Beats: 80.84%] - LeetPush

---
+++ 30DaysOfJavaScript/2704-To-Be-Or-Not-To-Be.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function (val) {
+    return {
+        toBe: (b) => {
+            if (b === val) {
+                return true
+            } else { throw 'Not Equal' }
+        },
+        notToBe: (a) => {
+            if (a !== val) {
+                return true
+            } else { throw 'Equal' }
+        }
+    }
+}
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
